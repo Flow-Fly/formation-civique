1f3c1ec9327dc5f09414482149b5787ae176f2d1
Florian <florian.aube@gmail.com>
2026-02-09 21:39:15 +0100

    Remove index.html file as part of project restructuring

--- app-react/vite.config.ts
+++ app-react/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/formation-civique/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
